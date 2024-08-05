cf7ee37881ef861414580817798b3e2620c97309
Rafael <Rafael@DESKTOP-PVS7EKS>
2024-08-05 19:55:10 -0300

    criação da interface Department e Seller Dao

--- projeto-dao/src/model/dao/DepartmentDao.java
+++ projeto-dao/src/model/dao/DepartmentDao.java
@@ -1,5 +1,14 @@
 package model.dao;
 
-public interface DepartmentDao {
+import java.util.List;
 
+import model.entities.Department;
+
+public interface DepartmentDao {
+	
+	void insert(Department obj);
+	void update(Department obj);
+	void deleteById(Integer id);
+	Department findById(Integer id);//responsável por consultar o id no banco de dados, se não existir retorna nulo
+	List<Department> findAll();//retorna todos os departamento
 }
--- projeto-dao/src/model/dao/SellerDao.java
+++ projeto-dao/src/model/dao/SellerDao.java
@@ -1,5 +1,15 @@
 package model.dao;
 
+import java.util.List;
+
+import model.entities.Seller;
+
 public interface SellerDao {
+	
+	void insert(Seller obj);
+	void update(Seller obj);
+	void deleteById(Integer id);
+	Seller findById(Integer id);//responsável por consultar o id no banco de dados, se não existir retorna nulo
+	List<Seller> findAll();//retorna todos os vendedores
 
 }
