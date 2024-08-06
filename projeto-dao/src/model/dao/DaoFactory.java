fb80012c979119b1ae15a9936995d758d8338708
Rafael <Rafael@DESKTOP-PVS7EKS>
2024-08-05 22:12:43 -0300

    implementação do findById

--- projeto-dao/src/model/dao/DaoFactory.java
+++ projeto-dao/src/model/dao/DaoFactory.java
@@ -1,12 +1,13 @@
 //classe auxiliar com operções staticas para instânciar os meus daos
 package model.dao;
 
+import db.DB;
 import model.dao.impl.SellerDaoJDBC;
 
 public class DaoFactory {
 	
 	public static SellerDao createSellerDao() {
-		return new SellerDaoJDBC();
+		return new SellerDaoJDBC(DB.getConnection());
 	}
 
 }
